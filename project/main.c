fb9384cdabf03a880164f6ecf09bb04d43da5e7b
adiaz50 <adiaz50@miners.utep>
2020-11-01 21:54:54 -0700

    Makefile not working and tons of bugs0

--- project/main.c
+++ project/main.c
@@ -1,31 +1,26 @@
 #include <msp430.h>
-
 #include "libTimer.h"
-
 #include "led.h"
-
 #include "buzzer.h"
-
 #include "stateMachines.h"
-
 
 
 int main(void) {
 
-  configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
-
-  switch_init();
+  configureClocks();
 
   led_init();
 
+  switch_init();
+
   buzzer_init();
 
+  buzzer_set_period(1000);
   
 
-  enableWDTInterrupts();	/* enable periodic interrupt */
+  enableWDTInterrupts();
 
 
-
-  or_sr(0x18);		/* CPU off, GIE on */
+  or_sr(0x18);
 
 }
